a43e08d14b063eabec84cc9ac6a5e69d7d103165
Jordan Steinberg <jordan4700@gmail.com>
2024-10-13 21:09:09 -0500

    enter to dashboard

--- app/(marketing)/_components/heading.tsx
+++ app/(marketing)/_components/heading.tsx
@@ -53,7 +53,7 @@ export const Heading = () => {
         )}
         {isAuthenticated && !isLoading &&(
           <Button asChild>
-            <Link href="/documents">
+            <Link href="/dashboard">
               Enter Citrus Reach
               <ArrowRight className="h-4 w-4 ml-2" />
             </Link>
