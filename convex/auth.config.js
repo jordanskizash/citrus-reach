4063d73fd3fc68b18e1f4016a359599e4d363872
Jordan Steinberg <jordan4700@gmail.com>
2024-11-04 23:38:17 -0600

    Fix readOnlyVideo component, profile page metadata, reply button

--- convex/auth.config.js
+++ convex/auth.config.js
@@ -3,7 +3,7 @@ export default {
         {
             //dev: https://awake-elf-68.clerk.accounts.dev
             //prod: https://clerk.citrusreach.com
-            domain: "https://awake-elf-68.clerk.accounts.dev",
+            domain: "https://clerk.citrusreach.com",
             applicationID: "convex",
         }
     ]
